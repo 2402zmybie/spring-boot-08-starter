e8c635760b7af4bcb6ec8b058861fbc94acc0c65
Lc <641229781@qq.com>
2020-12-24 09:07:52 +0800

    自定义starter

---
+++ hr-spring-boot-starter-autoconfigure/src/main/java/com/hr/hrspringbootstarterautoconfigure/HelloService.java
@@ -0,0 +1,18 @@
+package com.hr.hrspringbootstarterautoconfigure;
+
+public class HelloService {
+
+    private HelloProperties helloProperties;
+
+    public HelloProperties getHelloProperties() {
+        return helloProperties;
+    }
+
+    public void setHelloProperties(HelloProperties helloProperties) {
+        this.helloProperties = helloProperties;
+    }
+
+    public String sayHello(String name) {
+        return helloProperties.getPrefix() + "-" + name + "-" + helloProperties.getSuffix();
+    }
+}
